9e2d4e373f141ff3ad2b8b8168bbd6cdf165c237
MagePro310 <102444570+MagePro310@users.noreply.github.com>
2023-11-01 20:48:20 +0700

    a

---
+++ Bai2/Core/Inc/sofware_timer.h
@@ -0,0 +1,16 @@
+/*
+ * sofware_timer.h
+ *
+ *  Created on: Nov 1, 2023
+ *      Author: Dell
+ */
+
+#ifndef INC_SOFWARE_TIMER_H_
+#define INC_SOFWARE_TIMER_H_
+
+extern int timer1_flag;
+
+void setTimer1(int duration);
+void timerRun();
+
+#endif /* INC_SOFWARE_TIMER_H_ */
